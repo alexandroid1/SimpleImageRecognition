6db9529c3bdfe1e37080d40a0b4a33f5b2a94194
alexandroid1 <alex123456>
2018-02-05 15:27:57 +0200

    findBrightnessBorders

--- src/com/globallogic/Main.java
+++ src/com/globallogic/Main.java
@@ -7,16 +7,55 @@ import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.List;
 
 public class Main {
+    static int min;
+    static int max;
 
     public static void main(String[] args) throws IOException {
         verticalSort();
+        findBrightnessBorders();
+
+
+
         return;
     }
 
+    private static void findBrightnessBorders() throws IOException {
+        File file = new File ("linesVerticalSorted.png");
+        BufferedImage image = ImageIO.read(file);
+        WritableRaster raster = image.getRaster();
+
+        List<Integer> pixel0 = new ArrayList<>();
+        List<Integer> pixel1 = new ArrayList<>();
+        List<Integer> pixel2 = new ArrayList<>();
+
+        findMinMaxBrigth(raster, pixel0, pixel1, pixel2);
+
+        min = pixel0.get(0) + pixel1.get(0) + pixel2.get(0);
+        max = pixel0.get(raster.getWidth()-1) + pixel1.get(raster.getWidth()-1) + pixel2.get(raster.getWidth()-1);
+
+        System.out.println("min = " + min);
+        System.out.println("max = " + max);
+    }
+
+    private static void findMinMaxBrigth(WritableRaster raster, List<Integer> pixel0, List<Integer> pixel1, List<Integer> pixel2) {
+        for (int i = 0; i < raster.getWidth(); i++) {
+            int[] pixel = raster.getPixel(i, 0, new int[4]);
+            pixel0.add(pixel[0]);
+            pixel1.add(pixel[1]);
+            pixel2.add(pixel[2]);
+        }
+
+        Collections.sort(pixel0);
+        Collections.sort(pixel1);
+        Collections.sort(pixel2);
+    }
+
     private static void verticalSort() throws IOException {
-        BufferedImage image = ImageIO.read(Main.class.getResource("lines.png"));
+        File file = new File ("lines.png");
+        BufferedImage image = ImageIO.read(file);
         WritableRaster raster = image.getRaster();
 
         for (int i = 0; i < raster.getWidth(); i++) {
